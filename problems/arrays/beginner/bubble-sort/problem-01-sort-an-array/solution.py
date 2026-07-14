3666b4c578448ae0700641b7989667577b9f3f69
Sayantan Paul <paulsayantan2022@gmail.com>
2026-07-14 14:21:12 +0530

    Solve arrays\beginner\bubble-sort\problem-01-sort-an-array
    Submitted 2026-07-14 14:15:28 — 3 case(s) passing.

--- problems/arrays/beginner/bubble-sort/problem-01-sort-an-array/solution.py
+++ problems/arrays/beginner/bubble-sort/problem-01-sort-an-array/solution.py
@@ -17,8 +17,13 @@ class Solution:
             >>> Solution().sortArray([5, 2, 3, 1])
             [1, 2, 3, 5]
         """
-        # TODO: implement
-        pass
+        # implement bubble sort
+        for _ in range(len(nums)):
+            for j in range(len(nums)-1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+
+        return nums
 
 
 if __name__ == "__main__":
